14a7c816c87a7567d1f27f9b4f81998868f268fb
Alidoandres <141370844+Alidoandres@users.noreply.github.com>
2023-08-10 20:03:50 -0300

    todo de nuevo
    TAPRONTO; tuki

--- js/validacion.js
+++ js/validacion.js
@@ -5,20 +5,24 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
-document.getElementById("regBtn").addEventListener("click", function () {
- const formLabel = document.getElementsByClassName("form-control")
-   let texto = 0
- for (let i = 0; i < formLabel.length; i++) {
-    const element = formLabel[i];
-    if (element.value !== "") {
-        texto++;
+document.getElementById('regBtn').addEventListener('click', function() {
+    event.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+    var aceptoTerminos = document.getElementById('terminos').checked;
+  
+    if ((nombre === '' || apellido === '' || email === '' || password1 === '' || password2 === '' ) 
+        || (password1.length < 6 || password2.length < 6) 
+        || (password1 !== password2) 
+        || (!aceptoTerminos)) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
     }
- }
- if (texto >= 5) {
-    showAlertSuccess()
- } else {
-    showAlertError()
- }})
-
-
-
+    setTimeout(function() {
+        location.reload();
+      }, 3000);
+  })
