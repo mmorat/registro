e068986e25f86e63139899dd5ca99091a784bcab
rzolsc <renzo.lescano@hotmail.com>
2023-08-10 19:55:59 -0300

    codigo con validaciones

--- js/validacion.js
+++ js/validacion.js
@@ -5,20 +5,24 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
-document.getElementById("regBtn").addEventListener("click", function () {
- const formLabel = document.getElementsByClassName("form-control")
-   let texto = 0
- for (let i = 0; i < formLabel.length; i++) {
-    const element = formLabel[i];
-    if (element.value !== "") {
-        texto++;
+
+document.getElementById('regBtn').addEventListener('click', function() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+    var aceptoTerminos = document.getElementById('terminos').checked;
+  
+    if (nombre === '' || apellido === '' || email === '' || password1 === '' || password2 === '' ) {
+        showAlertError();
+    } else if (password1.length < 6 || password2.length < 6) {
+        showAlertError();
+    } else if (password1 !== password2) {
+        showAlertError();
+    } else if (!aceptoTerminos) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
     }
- }
- if (texto >= 5) {
-    showAlertSuccess()
- } else {
-    showAlertError()
- }})
-
-
-
+  })
